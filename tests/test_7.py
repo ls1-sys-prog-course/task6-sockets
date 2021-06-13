7fb1bbd8f0e9be4fad5dd7a9aaa6364dce9b3507
dgiantsidi <dimitra.giantsidi@gmail.com>
2021-06-13 20:48:59 +0300

    fix test_7

--- tests/test_7.py
+++ tests/test_7.py
@@ -26,6 +26,11 @@ def main() -> None:
 
                 proc.kill();
                 outs, errs = proc.communicate()
+                Lista = [x for x in outs.decode('utf-8').replace('\\n', '\n').split('\n') if x!='']
+                Lista = list(map(int, Lista))
+                if not Lista or len(Lista) != 339:
+                    warn(f"Server did not executed correctly")
+                    sys.exit(2)
 
                 info("OK")
 
